f38d2b2ce6286b1b710d29cea19e3ae084b353e1
MASSIMO2 <massimo.digi@hotmail.com>
2021-03-08 14:56:08 -0500

    added all the exception classes

---
+++ music_store/src/main/java/com/beatchamber/controller/exceptions/PreexistingEntityException.java
@@ -0,0 +1,12 @@
+package com.beatchamber.controller.exceptions;
+
+public class PreexistingEntityException extends Exception {
+
+    public PreexistingEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public PreexistingEntityException(String message) {
+        super(message);
+    }
+}
