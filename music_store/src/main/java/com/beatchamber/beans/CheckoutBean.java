651bb89bae7cb339532564e1518f3ac76fe1f5e7
Ibrah Kebe <kibra@outlook.com>
2021-03-24 14:38:08 -0400

    all of my validation is done all that is missing for my checkout page is the connection to the database to be able to add the orders in and I have also made the order tables

--- music_store/src/main/java/com/beatchamber/beans/CheckoutBean.java
+++ music_store/src/main/java/com/beatchamber/beans/CheckoutBean.java
@@ -17,63 +17,75 @@ import javax.inject.Named;
 @RequestScoped
 public class CheckoutBean implements Serializable {
     
-    private String CardNumber = "";
-    private String ExpDate = "";
-    private String Cvv = "";
-    private String Name = "";
-    private String Address = "";
-    private String City = "";
+    private String cardNumber = "";
+    private String expDate = "";
+    private String cvv = "";
+    private String name = "";
+    private String address = "";
+    private String city = "";
+    private String province = "-1";
     
+    /*all of the setters */
     public void setCardNumber(String value){
-        this.CardNumber=value;
+        this.cardNumber=value;
     }
     
     public void setDate(String value){
-        this.ExpDate=value;
+        this.expDate=value;
     }
     
     public void setCvv(String value){
-        this.Cvv=value;
+        this.cvv=value;
     }
     
     public void setName(String value){
-        this.Name=value;
+        this.name=value;
     }
     
     public void setAddress(String value){
-        this.Address=value;
+        this.address=value;
     }
     
     public void setCity(String value){
-        this.City=value;
+        this.city=value;
+    }
+    
+    public void setProvince(String value){
+        this.province = value;
+    }
+    
+    /*all of the getters*/
+    public String getProvince(){
+        return this.province;
     }
     
     public String getCardNumber(){
-        return this.CardNumber;
+        return this.cardNumber;
     }
     
     public String getCvv(){
-        return this.Cvv;
+        return this.cvv;
     }
     
     public String getName(){
-        return this.Name;
+        return this.name;
     }
     
     public String getAddress(){
-        return this.Address;
+        return this.address;
     }
     
     public String getCity(){
-        return this.City;
+        return this.city;
     }
     
     public String getDate(){
-        return this.ExpDate;
+        return this.expDate;
     }
     
+    /*overiding the toString method*/
     @Override
     public String toString() {
-        return this.CardNumber;
+        return "pop";
     }
 }
