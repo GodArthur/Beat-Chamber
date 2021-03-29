6335ca7d003585a9ef2cac9e00a8b8d3407d2e83
Susan Vuu <supermegas03@gmail.com>
2021-03-29 14:40:56 -0400

    Locale changer still partially works, changed the album_list name to browse_music

--- music_store/src/main/java/com/beatchamber/beans/LocaleChanger.java
+++ music_store/src/main/java/com/beatchamber/beans/LocaleChanger.java
@@ -1,7 +1,9 @@
 package com.beatchamber.beans;
 
+import java.io.Serializable;
 import java.util.Locale;
 import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
 
@@ -9,55 +11,33 @@ import javax.inject.Named;
  * Class to change the language of the website
  * 
  * @author Susan Vuu - 1735488
- * Original code provided by Ken Fogel
  */
 @Named
-@RequestScoped
-public class LocaleChanger {
-    
-    private boolean english;
-    private boolean french;
+@SessionScoped
+public class LocaleChanger implements Serializable {
     
     public LocaleChanger(){
-        english = true;
-        french = false;
-    }
-
-    public boolean isEnglish() {
-        return english;
-    }
-
-    public void setEnglish(boolean english) {
-        this.english = english;
-    }
-
-    public boolean isFrench() {
-        return french;
-    }
-
-    public void setFrench(boolean french) {
-        this.french = french;
+        
     }
     
+    /**
+     * Changes the language of the website depending on the current one
+     * @return The same page the user was currently on
+     */
     public String changeLocale() {
-        FacesContext context = FacesContext.getCurrentInstance();   
-
-        Locale aLocale;
-        if (english) {
-            aLocale = Locale.CANADA_FRENCH;
-            english = false;
-            french = true;
+        FacesContext context = FacesContext.getCurrentInstance();
+        Locale currentLocale = context.getViewRoot().getLocale();
+        
+        if (currentLocale == Locale.CANADA || currentLocale == Locale.ENGLISH) {
+            context.getViewRoot().setLocale(Locale.CANADA_FRENCH);
         }
-        else if (french) {
-            aLocale = Locale.CANADA;
-            french = false;
-            english = true;
+        else if (currentLocale == Locale.CANADA_FRENCH || currentLocale == Locale.FRENCH) {
+            context.getViewRoot().setLocale(Locale.CANADA);
         }
         else {
-            aLocale = Locale.getDefault();
+            context.getViewRoot().setLocale(Locale.getDefault());
         }
         
-        context.getViewRoot().setLocale(aLocale);
         return null;
     }
 }
