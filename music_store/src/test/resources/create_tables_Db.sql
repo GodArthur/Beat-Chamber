b2ecff955b8f0979013b86d36fee8fefdbb2d99e
Ibrah Kebe <kibra@outlook.com>
2021-03-29 14:41:52 -0400

    fixed smalle problems in the database

--- music_store/src/test/resources/create_tables_Db.sql
+++ music_store/src/test/resources/create_tables_Db.sql
@@ -8,7 +8,7 @@ DROP USER IF EXISTS dawson@localhost;
 CREATE USER dawson@'localhost' IDENTIFIED WITH mysql_native_password BY 'dawson123' REQUIRE NONE;
 GRANT ALL ON beat_chamber.* TO dawson@'localhost';*/
 
-
+/*
 DROP DATABASE IF EXISTS CSgb1w21;
 CREATE DATABASE CSgb1w21;
 USE CSgb1w21;
@@ -20,15 +20,15 @@ CREATE USER CSgb1w21@'%' IDENTIFIED WITH mysql_native_password BY 'odeckoxb' REQ
 GRANT ALL ON CSgb1w21.* TO CSgb1w21@'localhost';
 
 GRANT ALL ON CSgb1w21.* TO CSgb1w21@'%';
-
+*/
 
 
 
 /*remove tables if they are already present */
 
-/*drop database CSgb1w21;
+rop database CSgb1w21;
 CREATE DATABASE CSgb1w21;
-USE CSgb1w21;*/
+USE CSgb1w21;
 
 /*DROP TABLE IF EXISTS Ads;
 DROP TABLE IF EXISTS RSS_Feeds;
@@ -157,7 +157,6 @@ cell_phone varchar(20),
 email varchar(40) not null,
 genre_of_last_search varchar(40),
 username varchar(40) not null,
-password varchar(40) not null,
 salt varchar(32),
 hash varchar(32)
 );
