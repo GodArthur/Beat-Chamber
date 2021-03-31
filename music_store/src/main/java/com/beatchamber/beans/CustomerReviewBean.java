0827b76b9a8e027bbc3553cadce773a7b8994db9
MASSIMO2 <massimo.digi@hotmail.com>
2021-03-31 18:19:28 -0400

    working on adding review to db

--- music_store/src/main/java/com/beatchamber/beans/CustomerReviewBean.java
+++ music_store/src/main/java/com/beatchamber/beans/CustomerReviewBean.java
@@ -1,82 +1,141 @@
 package com.beatchamber.beans;
 
+import com.beatchamber.beans.manager.ClientsBackingBean;
+import com.beatchamber.beans.manager.SurveysBackingBean;
+import com.beatchamber.entities.CustomerReviews;
+import com.beatchamber.exceptions.RollbackFailureException;
+import com.beatchamber.jpacontroller.CustomerReviewsJpaController;
+import com.beatchamber.jpacontroller.ClientsJpaController;
+import com.beatchamber.jpacontroller.TracksJpaController;
 import java.io.Serializable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.sql.Date;
+import java.util.List;
+import java.util.logging.Level;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
 
 /**
- *This bean will be used to obtain information for sending a review to the database
- * and it will be used to keep track of what track the client is reviewing.
- * 
+ * This bean will be used to obtain information for sending a review to the
+ * database and it will be used to keep track of what track the client is
+ * reviewing.
+ *
  * @author Massimo Di Girolamo
  */
+@Named("customerReviews")
+@SessionScoped
 public class CustomerReviewBean implements Serializable {
 
     private final static Logger LOG = LoggerFactory.getLogger(CustomerReviewBean.class);
-    
+
+    @Inject
+    private CustomerReviewsJpaController customerReviewsJpaController;
+
+    @Inject
+    private ClientsJpaController clientsJpaController;
+
+    @Inject
+    private TracksJpaController tracksJpaController;
+
+    @Inject
+    private ClientsBackingBean clientsBackingBean;
+
+    @Inject
+    private TrackBean trackBean;
+
+    private List<CustomerReviews> customerReviews;
+
+    private CustomerReviews selectedCustomerReviews;
+
     private int track_id;
     private int client_number;
     private Date review_date;
     private String rating;
     private String review_text;
     private boolean approval_status;
-    
-    public CustomerReviewBean(int track_id, int client_number, Date review_date, String rating, String review_text, boolean approval_status){
-        
-        this.track_id = track_id;
-        this.client_number = client_number;
-        this.review_date = review_date;
-        this.rating = rating;
-        this.review_text = review_text;
-        this.approval_status = approval_status;
-    }
-    
-    public int getTrack_id(){
+
+    /*@PostConstruct
+    public void init() {
+        this.customerReviews = customerReviewsJpaController.findCustomerReviewsEntities();
+    }*/
+
+    public int getTrack_id() {
         return this.track_id;
     }
-    
-    public void setTrack_id(Integer trackId){
+
+    public void setTrack_id(Integer trackId) {
         this.track_id = trackId;
     }
-    
-    public int getClient_number(){
+
+    public int getClient_number() {
         return this.client_number;
     }
-    
-    public void setClient_number(int clientNumber){
+
+    public void setClient_number(int clientNumber) {
         this.client_number = clientNumber;
     }
-    
-    public Date getReview_date(){
+
+    public Date getReview_date() {
         return this.review_date;
     }
-    
-    public void setReview_date(Date reviewDate){
+
+    public void setReview_date(Date reviewDate) {
         this.review_date = reviewDate;
     }
-    
-    public String getRating(){
+
+    public String getRating() {
         return this.rating;
     }
-    
-    public void setRating(String rating){
+
+    public void setRating(String rating) {
         this.rating = rating;
     }
-    
-    public String getReview_Text(){
+
+    public String getReview_Text() {
         return this.review_text;
     }
-    
-    public void setReview_Text(String reviewText){
+
+    public void setReview_Text(String reviewText) {
         this.review_text = reviewText;
     }
-    
-    public boolean getApprovalStatus(){
+
+    public boolean getApprovalStatus() {
         return this.approval_status;
     }
-    
-    public void setApprovalStatus(boolean approvStatus){
+
+    public void setApprovalStatus(boolean approvStatus) {
         this.approval_status = approvStatus;
     }
+
+    public void addCustomerReview() throws RollbackFailureException {
+
+        try {
+            selectedCustomerReviews.setTrackId(tracksJpaController.findTracks(trackBean.getTrackId()));
+            selectedCustomerReviews.setClientNumber(clientsJpaController.findClients(clientsBackingBean.getSelectedClient().getClientNumber()));
+            selectedCustomerReviews.setReviewDate(new java.sql.Date(System.currentTimeMillis()));
+
+            //this.rating and this.review should be set by user on site 
+            selectedCustomerReviews.setApprovalStatus(false);
+
+            //If the rating and the review are filled in then review can be made
+            if (this.rating != null && this.review_text.isEmpty()) {
+
+                LOG.trace("Creating new review entity object");
+                customerReviewsJpaController.create(selectedCustomerReviews);
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Review Added! It will be shown once it is approved"));
+            } else {
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("The review must have a rating and a written review"));
+            }
+
+        } catch (RollbackFailureException ex) {
+            java.util.logging.Logger.getLogger(SurveysBackingBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
 }
